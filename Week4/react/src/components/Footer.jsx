b25500eae3a91c19acda8937ceea65da22bae7d5
simrannair03 <114979834+simrannair03@users.noreply.github.com>
2025-04-11 14:25:36 +0530

    Create Footer.jsx

---
+++ Week4/react/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+      <footer className="bg-blue-500 text-white text-center p-4 mt-10">
+        <p>&copy; 2025 MyStore. All rights reserved.</p>
+        <div className="mt-2">
+          <a href="#" className="mx-2">Facebook</a>
+          <a href="#" className="mx-2">Twitter</a>
+          <a href="#" className="mx-2">Instagram</a>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
